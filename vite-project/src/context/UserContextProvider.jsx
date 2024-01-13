5b2de5794d749db54a8f87af76c4025ce65940d7
Iqra Khaliq <iqra44938@gmail.com>
2024-01-12 22:00:13 -0800

    error

--- vite-project/src/context/UserContextProvider.jsx
+++ vite-project/src/context/UserContextProvider.jsx
@@ -1,13 +1,13 @@
+import React from "react";
+import UserContext from "./UserContext";
 
-import { useState } from "react"
-import UserContext from "./UserContext"
-const UserContextProvider  = ({elements})=>{
-    const [user,setUser]   = useState(null);
-   return(
-    <UserContext.Provider value = {{user,setUser}}>
-  {elements}
-
-    </UserContext.Provider>
-   )
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null)
+    return(
+        <UserContext.Provider value={{user, setUser}}>
+        {children}
+        </UserContext.Provider>
+    )
 }
-export default UserContextProvider;
+
+export default UserContextProvider
